e9ad0603caec515a6249c6d26744743f9427eb0d
destiny32601 <destiny32601@gmail.com>
2023-04-13 22:21:40 -0700

    added some of the code for the index.js file, updated my node and inquierer versions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const inquier = require('inquirer');
+
+const writeFile = require('fs').promises.writeFile;
+
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of your project',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter the description of your project',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter the usage information for your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter the installation instructions for project',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose your License',
+            choices: ['None', 'Apache 2.0', 'MIT', 'GPL 3.0', 'BSD 3-Clause', 'Boost 1.0', 'MPL 3.0', 'CC0 1.0']
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Enter contributions for your project',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter the test instructions for your project',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email',
+        },
+    ]
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { 
+    return writeFile(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    userPrompt()
+    .then((answers)=> writeFile('./generated_README/README.md', generateMarkdown(answers)))
+    .then(() => console.log('Successfully generated readme to /generated_README/README.md'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
